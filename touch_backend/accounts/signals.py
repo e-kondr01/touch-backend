09606f575288a39d93fcf2f38696404966f731d9
e-kondr01 <e.kondr01@gmail.com>
2021-02-28 15:16:05 +0300

    post save signal

---
+++ touch_backend/accounts/signals.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from cards.models import Card
+
+
+@receiver(post_save, sender=User, dispatch_uid="create_card")
+def create_card(sender, instance, **kwargs):
+    new_card = Card(owner=instance, page_path=instance.username)
+    new_card.save()
