73376c982436ff3602a3687b57b2e0a2f2ad92d0
e-kondr01 <e.kondr01@gmail.com>
2021-03-04 15:04:47 +0300

    url

--- touch_backend/touch_backend/urls.py
+++ touch_backend/touch_backend/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('api/admin/', admin.site.urls),
-    path('api/', include('cards.urls')),
-    path('api/accounts/', include('accounts.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('cards.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
