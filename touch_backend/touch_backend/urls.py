4c89519d099640c397ba63e8ae397b10d8f9c024
e-kondr01 <e.kondr01@gmail.com>
2021-03-04 15:02:22 +0300

    https nginx?

--- touch_backend/touch_backend/urls.py
+++ touch_backend/touch_backend/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/', include('cards.urls')),
     path('api/accounts/', include('accounts.urls')),
 ]
