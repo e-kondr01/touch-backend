25a1090c361587817ac719585602eb3f70068873
Egor Kondrashov <e.kondr01@gmail.com>
2021-08-17 20:31:07 +0300

    fix: add migration

---
+++ touch_backend/cards/migrations/0009_auto_20210817_2021.py
@@ -0,0 +1,22 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("cards", "0008_auto_20210512_2116"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="card",
+            name="has_changed_username",
+            field=models.BooleanField(defaul=False, verbose_name="менял логин")
+        ),
+        migrations.AddField(
+            model_name="card",
+            name="qr",
+            field=models.ImageField(blank=True, null=True, upload_to="",
+                                    verbose_name="QR код"),
+        ),
+    ]
